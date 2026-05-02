ed6c5fa3c4704bfa16f22f800e03d860b3fabe1d
Beatriz Martins <bea99martins@gmail.com>
2026-05-02 17:25:39 -0300

    adição das skills, incluindo icons. atualização da foto e do card aboutme

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,26 +1,53 @@
 import aboutme from '../../image/aboutme.png';
 
+import { IoLogoJavascript } from 'react-icons/io5';
+import { FaReact } from 'react-icons/fa6';
+import { IoLogoFigma } from 'react-icons/io5';
+import { IoLogoHtml5 } from 'react-icons/io';
+import { FaCss3Alt } from 'react-icons/fa';
+import { FaGithubAlt } from 'react-icons/fa';
+
 import * as about from './styled';
 
 export default function About() {
   return (
     <about.Aboutdiv>
-      <about.AboutContent>
-        <about.AboutImage src={aboutme} />
-        <about.Me>Sobre Mim</about.Me>
-        <about.BoxAbout>
-          <about.AboutText>
-            Sou desenvolvedora front-end, apaixonada por criar interfaces
-            intuitivas e visualmente agradáveis.
-            <about.SecondText>
-              Tenho foco em transformar ideias em experiências digitais
-              funcionais, unindo código e design de forma equilibrada.
-              Atualmente, estou aprofundando meus conhecimentos em UX/UI,
-              JavaScript e React, buscando evoluir continuamente a cada projeto.
-            </about.SecondText>
-          </about.AboutText>
-        </about.BoxAbout>
-      </about.AboutContent>
+      <about.AboutRow>
+        <about.Left>
+          <about.AboutImage src={aboutme} />
+        </about.Left>
+
+        <about.Right>
+          <about.Me>Sobre Mim</about.Me>
+          <about.BoxAbout>
+            <div>
+              <about.AboutText>
+                Sou desenvolvedora front-end, apaixonada por criar interfaces
+                intuitivas e visualmente agradáveis.
+              </about.AboutText>
+
+              <about.SecondText>
+                Tenho foco em transformar ideias em experiências digitais
+                funcionais, unindo código e design de forma equilibrada.
+                Atualmente, estou aprofundando meus conhecimentos em UX/UI,
+                JavaScript e React, buscando evoluir continuamente a cada
+                projeto.
+              </about.SecondText>
+            </div>
+          </about.BoxAbout>
+          <about.TitleSkills>Skills</about.TitleSkills>
+          <about.SkillsSection>
+            <about.CardSkills>
+              <IoLogoJavascript className="js" />
+              <IoLogoFigma className="figma" />
+              <FaReact className="react" />
+              <FaCss3Alt className="css" />
+              <IoLogoHtml5 className="html" />
+              <FaGithubAlt className="github" />
+            </about.CardSkills>
+          </about.SkillsSection>
+        </about.Right>
+      </about.AboutRow>{' '}
     </about.Aboutdiv>
   );
 }
--- src/pages/about/styled.jsx
+++ src/pages/about/styled.jsx
@@ -19,18 +19,42 @@ export const Aboutdiv = styled.div`
 `;
 
 export const AboutImage = styled.img`
-  width: 468px;
-  height: 480px;
-  object-fit: cover;
-  margin-top: 90px;
+  width: 490px;
+  height: 490px;
+  object-fit: contain;
+  margin-top: 80px;
+  border-radius: 20px;
+  border: 1px solid rgba(176, 122, 158, 0.2);
+  transition: 0.3s;
+
+  &:hover {
+    transform: scale(1.05);
+    transition: all 0.3s ease;
+  }
 `;
 
-export const AboutContent = styled.article`
+export const AboutRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 60px;
+  width: 100%;
+  max-width: 1100px;
+`;
+
+export const Left = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: -90px;
+`;
+
+export const Right = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  gap: 4px;
-  margin-top: -90px;
+  gap: 20px;
+  max-width: 700px;
+  margin-right: -90px;
 `;
 
 export const Me = styled.h1`
@@ -38,7 +62,7 @@ export const Me = styled.h1`
   font-weight: 400;
   font-size: 40px;
   color: #b07a87;
-  margin-top: -102px;
+  margin-top: 30px;
 `;
 
 export const AboutText = styled.p`
@@ -46,7 +70,8 @@ export const AboutText = styled.p`
   font-weight: 400;
   color: #b07a87;
   font-size: 18px;
-  text-align: center;
+  text-align: left;
+  line-height: 1.7;
 `;
 
 export const SecondText = styled.p`
@@ -54,12 +79,14 @@ export const SecondText = styled.p`
   font-weight: 300;
   color: #b07a87;
   font-size: 18px;
-  line-height: 1.5;
-  text-align: center;
+  text-align: left;
+  opacity: 0.8;
+  line-height: 1.7;
 `;
 
 export const BoxAbout = styled.div`
-  width: 1000px;
+  max-width: 950px;
+  width: 100%;
   display: flex;
   flex: 1, 0, 60%;
   align-items: center;
@@ -70,4 +97,60 @@ export const BoxAbout = styled.div`
   background-color: #f6eef1;
   box-shadow: 2px 8px 12px rgba(176, 122, 135, 0.12);
   border: 2px solid rgba(176, 122, 135, 0.2);
+`;
+
+export const CardSkills = styled.div`
+  background: #f6eef1;
+  padding: 14px;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(176, 122, 135, 0.15);
+  border: 2px solid rgba(176, 122, 135, 0.2);
+`;
+
+export const TitleSkills = styled.h3`
+  margin-top: 60px;
+  font-family: 'DM Serif Display', serif;
+  font-weight: 400;
+  font-size: 40px;
+  color: #b07a87;
+`;
+
+export const SkillsSection = styled.div`
+  display: flex;
+  gap: 20px;
+
+  .js,
+  .react,
+  .figma,
+  .html,
+  .css,
+  .github {
+    font-size: 50px;
+    color: #5a3d44;
+    opacity: 0.8;
+    transition: all 0.3s ease;
+
+    &:hover {
+      transform: scale(1.15);
+    }
+  }
+
+  .js:hover {
+    color: #e4d147;
+  }
+  .react:hover {
+    color: #61dafb;
+  }
+  .html:hover {
+    color: #e34f26;
+  }
+  .css:hover {
+    color: #1572b6;
+  }
+  .figma:hover {
+    color: #a259ff;
+  }
+  .github:hover {
+    color: #171515;
+  }
 `;
